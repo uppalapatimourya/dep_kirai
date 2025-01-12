58cd0bebc6c4d602bb4259eb63a2e4d3e90cccb3
uppalapatimourya <uppalapatimourya49@gmail.com>
2025-01-12 20:23:13 +0530

    Added comments for APIs

--- src/main/java/com/kirai/controller/KiraiController.java
+++ src/main/java/com/kirai/controller/KiraiController.java
@@ -19,17 +19,34 @@ public class KiraiController {
     @Autowired
     private KiraiDetailsService kiraiDetailsService;
 
+    /**
+     * API Exposed to save the Kirai Detais
+     * @param kiraiDetailsDTO
+     * @return
+     */
     @PostMapping("/saveKiraiDetails")
     public ResponseEntity<String> saveKiraiDetails(@RequestBody KiraiDetailsDTO kiraiDetailsDTO){
         log.debug("Api Called :: saveKiraiDetails");
         return new ResponseEntity<>(kiraiDetailsService.saveKiraiDetails(kiraiDetailsDTO), HttpStatus.OK);
     }
 
+    /**
+     * API Exposed to Retrieve Kirai Details with pagination
+     * @param page
+     * @param size
+     * @return
+     */
     @GetMapping("/getKiraiDetails")
     public ResponseEntity<List<KiraiDetailsDTO>>  retrieveKiraiDetails(@RequestParam int page, @RequestParam int size){
         return new ResponseEntity<>(kiraiDetailsService.getKiraiDetails(page,size),HttpStatus.OK);
     }
 
+    /**
+     * Search Kirai Details using filed and value
+     * @param fieldName
+     * @param value
+     * @return
+     */
     @GetMapping("/filter")
     public ResponseEntity<List<KiraiDetailsDTO>> filterKiraiDetails(@RequestParam String fieldName,@RequestParam String value){
         return new ResponseEntity<>(kiraiDetailsService.filterKiraiDetails(fieldName, value),HttpStatus.OK);
